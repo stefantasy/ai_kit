c12edc8992db806d2858adaab949b4d4311631a6
stefantasy <stefan1992@qq.com>
2019-06-06 14:46:39 +0800

    delete some code unusefull

--- mx_bringup/Show/Demo/G.建立地图(激光雷达).sh
+++ mx_bringup/Show/Demo/G.建立地图(激光雷达).sh
@@ -10,9 +10,6 @@ echo G Maping Mode|rosrun sound_play say.py
 
 gnome-terminal -t "启动rbcBot机器人" -x bash -c "roslaunch mx_bringup rbc_lidar_start.launch ;exec bash;"
 
-sleep 1
-gnome-terminal -t "启动手柄控制" -x bash -c "roslaunch mx_teleop logitech.launch ;exec bash;"
-
 sleep 3
 gnome-terminal -t "启动Gmapping建图" -x bash -c "roslaunch mx_nav gmapping_demo.launch ;exec bash;"
 
--- mx_bringup/Show/Demo/J.深度跟随(深度相机).sh
+++ mx_bringup/Show/Demo/J.深度跟随(深度相机).sh
@@ -8,7 +8,7 @@ sleep 1.0
 
 echo P C L Follow Mode|rosrun sound_play say.py
 
-gnome-terminal -t "启动rbcBot机器人" -x bash -c "roslaunch mx_bringup rbc_show_start.launch ;exec bash;"
+gnome-terminal -t "启动rbcBot机器人" -x bash -c "roslaunch mx_bringup rbc_camera_start.launch ;exec bash;"
 
 sleep 2.5
 gnome-terminal -t "启动PCL点云追踪follower" -x bash -c "roslaunch mx_apps follower2.launch ;exec bash;"
